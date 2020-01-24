4d9196c8bd68654ea36c74e953277ad698a45552
v-flores <44541135+v-flores@users.noreply.github.com>
2020-01-23 18:24:33 -0600

    blog title fix

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,6 +6,9 @@ const PostList = ({ posts }) => {
   return (
     <section className={styles.posts}>
       <h1>Blog</h1>
+      <i>My journey in the the web development world alongside tutorials and other new tech findings.</i>
+      <br>
+      </br>
       <div className={styles.center}>
         {posts.map(({ node }, index) => {
           return <PostCard key={index} post={node} />
