d9ea2dc07cfe721615073d72cdae3036c790b4f2
v-flores <44541135+v-flores@users.noreply.github.com>
2020-02-04 22:17:30 -0600

    react blog post and graphql image query fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import GlobalContextProvider from './src/context/GlobalContextProvider';
+import React from "react"
+import GlobalContextProvider from "./src/context/GlobalContextProvider"
+
 
 export const wrapRootElement = ({ element }) => {
-    return (
-      <GlobalContextProvider>
-        {element}
-        </GlobalContextProvider>
-    )
-  }
+  return <GlobalContextProvider>{element}</GlobalContextProvider>
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,95 +4,99 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-const path = require('path');
+const path = require("path")
 
 module.exports = {
   siteMetadata: {
-    title: "Severus Snape",
+    title: "VictorDoes.Dev",
     titleTemplate: "VictorDoes.Dev - %s ",
     description:
       "This is the official website of the Chicago based Web Developer Victor Flores .",
     url: "https://www.victordoes.dev", // No trailing slash allowed!
     image: "/uploads/laptop.jpg", // Path to your image you placed in the 'static' folder
   },
-  pathPrefix: '/gatsby-cv-site',
+  pathPrefix: "/gatsby-cv-site",
   plugins: [
     {
-      resolve: 'gatsby-plugin-html-attributes',
+      resolve: "gatsby-plugin-html-attributes",
       options: {
-        lang: 'en',
-      }
+        lang: "en",
+      },
     },
- {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: path.join(__dirname, `src`, `images`),
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
     },
-    
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `portfolio`,
-      path: path.join(__dirname, `src`, `portfolio`),
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `portfolio`,
+        path: path.join(__dirname, `src`, `portfolio`),
+      },
     },
-    
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `posts`,
-      path: path.join(__dirname, `src`, `posts`),
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: path.join(__dirname, `src`, `posts`),
+      },
     },
-    
-  },
-  {
-    resolve: 'gatsby-plugin-mdx',
-    options: {
-      gatsbyRemarkPlugins: [ `gatsby-remark-images` ],
-      plugins: [ `gatsby-remark-images` ],
-    }
-  },
-  {
-    resolve: "gatsby-remark-external-links",
-    options: {
-      target: "_self",
-      rel: "nofollow"
-    }
-  },
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'src',
-      path: `${__dirname}/src/`
-    }
-  },
-  {
-    resolve: 'gatsby-transformer-remark',
-    options: {
-      plugins: [
-        'gatsby-remark-relative-images',
-        `gatsby-remark-copy-linked-files`,
-        {
-          resolve: 'gatsby-remark-images',
-          options: {
-            maxWidth: 750,
-            linkImagesToOriginal: false
-          }
-        },
-        {
-          resolve: `gatsby-remark-copy-linked-files`,
-          options: {
-            destinationDir: `path/to/dir`,
-            ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
-          }
-        }
-      ]
-    }
-},
-    'gatsby-plugin-sass',
-     `gatsby-transformer-sharp`, 
-     `gatsby-plugin-sharp`,
-     `gatsby-plugin-mdx`]
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [`gatsby-remark-images`],
+        plugins: [`gatsby-remark-images`],
+      },
+    },
+    {
+      resolve: "gatsby-remark-external-links",
+      options: {
+        target: "_self",
+        rel: "nofollow",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              destinationDir: `path/to/dir`,
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-sass",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [{ resolve: "gatsby-remark-images" }],
+      },
+    },
+  ],
 }
